41a4a646e5d71da791fd69cd981f151ea5a5f13a
aytachuseynli <aytac_ha@mail.ru>
2023-11-10 12:21:51 -0800

    101-strtow.c

--- malloc_free/101-strtow.c
+++ malloc_free/101-strtow.c
@@ -10,21 +10,22 @@
  */
 int number(char *str)
 {
-	int a, num = 0;
+    int a, num = 0;
 
-	for (a = 0; str[a] != '\0'; a++)
-	{
-		if (*str == ' ')
-			str++;
-		else
-		{
-			for (; str[a] != ' ' && str[a] != '\0'; a++)
-				str++;
-			num++;
-		}
-	}
-	return (num);
+    for (a = 0; str[a] != '\0'; a++)
+    {
+        if (*str == ' ')
+            str++;
+        else
+        {
+            for (; str[a] != ' ' && str[a] != '\0'; a++)
+                str++;
+            num++;
+        }
+    }
+    return (num);
 }
+
 /**
  * free_everything - frees the memory
  * @string: pointer values being passed for freeing
@@ -32,9 +33,52 @@ int number(char *str)
  */
 void free_everything(char **string, int i)
 {
-	for (; i > 0;)
-		free(string[--i]);
-	free(string);
+    for (; i > 0;)
+        free(string[--i]);
+    free(string);
+}
+
+/**
+ * split_word - Helper function to split a word from the string
+ * @str: Pointer to the current position in the string
+ * @length: Pointer to store the length of the word
+ *
+ * Return: The word (substring)
+ */
+char *split_word(char *str, int *length)
+{
+    char *word_start = str;
+    *length = 0;
+
+    while (*str != ' ' && *str != '\0')
+    {
+        (*length)++;
+        str++;
+    }
+
+    return word_start;
+}
+
+/**
+ * process_word - Helper function to process a word and allocate memory
+ * @word_start: Pointer to the start of the word
+ * @length: Length of the word
+ *
+ * Return: Allocated memory for the word
+ */
+char *process_word(char *word_start, int length)
+{
+    char *word = malloc((length + 1) * sizeof(char));
+
+    if (word == NULL)
+        return NULL;
+
+    for (int i = 0; i < length; i++)
+        word[i] = word_start[i];
+
+    word[length] = '\0';
+
+    return word;
 }
 
 /**
@@ -44,47 +88,44 @@ void free_everything(char **string, int i)
  */
 char **strtow(char *str)
 {
-	int total_words = 0, b = 0, c = 0, length = 0;
-	char **words, *found_word;
+    int total_words, b = 0, c = 0, length = 0;
+    char **words, *found_word;
 
-	if (str == 0 || *str == 0)
-		return (NULL);
-	total_words = number(str);
-	if (total_words == 0)
-		return (NULL);
-	words = malloc((total_words + 1) * sizeof(char *));
-	if (words == 0)
-		return (NULL);
-	for (; *str != '\0' &&  b < total_words;)
-	{
-		if (*str == ' ')
-			str++;
-		else
-		{
-			found_word = str;
-			for (; *str != ' ' && *str != '\0';)
-			{
-				length++;
-				str++;
-			}
-			words[b] = malloc((length + 1) * sizeof(char));
-			if (words[b] == 0)
-			{
-				free_everything(words, b);
-				return (NULL);
-			}
-			while (*found_word != ' ' && *found_word != '\0')
-			{
-				words[b][c] = *found_word;
-				found_word++;
-				c++;
-			}
-			words[b][c] = '\0';
-			b++;
-			c = 0;
-			length = 0; 
-			str++;
-		}
-	}
-	return (words);
+    if (str == NULL || *str == '\0')
+        return NULL;
+
+    total_words = number(str);
+
+    if (total_words == 0)
+        return NULL;
+
+    words = malloc((total_words + 1) * sizeof(char *));
+
+    if (words == NULL)
+        return NULL;
+
+    while (*str != '\0' && b < total_words)
+    {
+        if (*str == ' ')
+            str++;
+        else
+        {
+            found_word = split_word(str, &length);
+
+            words[b] = process_word(found_word, length);
+
+            if (words[b] == NULL)
+            {
+                free_everything(words, b);
+                return NULL;
+            }
+
+            b++;
+            c = 0;
+            str += length;
+        }
+    }
+
+    return words;
 }
+
