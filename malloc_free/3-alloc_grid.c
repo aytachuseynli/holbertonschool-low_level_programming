377cfccea1de3ff075d68a02d50401b9451337b1
aytachuseynli <aytac_ha@mail.ru>
2023-11-10 11:40:41 -0800

    up

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -11,7 +11,7 @@
 
 int **alloc_grid(int width, int height)
 {
-	int *grid;
+	int **grid;
 	int i, j;
 
 	if (width + height < 2 || width < 1 || height < 1)
@@ -34,5 +34,6 @@ int **alloc_grid(int width, int height)
 		for (j = 0; j < width; j++)
 			grid[i][j] = 0;
 	}
+
 	return (grid);
 }
