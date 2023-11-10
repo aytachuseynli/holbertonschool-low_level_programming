905102277980ec4819570fd28eda2305bf90ba84
aytachuseynli <aytac_ha@mail.ru>
2023-11-10 01:46:19 -0800

    1-strdup.c

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -10,7 +10,7 @@
  */
 
 char *create_array(unsigned int size, char c)
-	{
+{
 	char *a;
 	unsigned int i;
 
@@ -25,4 +25,4 @@ char *create_array(unsigned int size, char c)
 		a[i] = c;
 
 	return (a);
-	}
+}
