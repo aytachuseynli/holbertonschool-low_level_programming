e1145816d4e0e7e6e4e6f03ffc818e94feb5612b
aytachuseynli <aytac_ha@mail.ru>
2023-11-26 02:16:45 -0800

    100-hello_wor

--- variadic_functions/100-hello_world.asm
+++ variadic_functions/100-hello_world.asm
@@ -1,15 +1,17 @@
-    section .data
-msg:	db	"Hello, Holberton",10
-    global main
+global main
 
-    section .text
+section .text
 main:
-    mov	rax, 1
-    mov	rdi, 1
-    mov	rsi, msg
-    mov	rdx, 17
+    mov rax, 1       ; System call number for write
+    mov rdi, 1       ; File descriptor for standard output (stdout)
+    mov rsi, message ; Pointer to the message string
+    mov rdx, 13      ; Length of the message string, including the newline character
     syscall
 
-    mov rax, 60
-    xor rdi, rdi
+    mov rax, 60      ; System call number for exit
+    xor rdi, rdi     ; Exit code 0
     syscall
+
+section .data
+message: db "Hello, World", 10 ; The message string with a newline character
+
