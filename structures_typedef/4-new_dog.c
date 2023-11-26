457db0b735384ead2dde945571356f1169aaf5ef
aytachuseynli <aytac_ha@mail.ru>
2023-11-26 02:27:12 -0800

    4-new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -12,31 +12,30 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    dog_t *p;
+	dog_t *p;
 
-    /* Reserve memory for the dog structure */
-    p = malloc(sizeof(dog_t));
-    if (p == NULL)
-        return (NULL);
+	/* Reserve memory for the dog structure */
+	p = malloc(sizeof(dog_t));
+	if (p == NULL)
+		return (NULL);
 
-    /* Allocate memory for name and owner, and copy the content */
-    p->name = malloc(strlen(name) + 1);
-    p->owner = malloc(strlen(owner) + 1);
+	/* Allocate memory for name and owner, and copy the content */
+	p->name = malloc(strlen(name) + 1);
+	p->owner = malloc(strlen(owner) + 1);
 
-    /* Check for allocation failures */
-    if (p->name == NULL || p->owner == NULL)
-    {
-        free(p->name);
-        free(p->owner);
-        free(p);
-        return (NULL);
-    }
+	/* Check for allocation failures */
+	if (p->name == NULL || p->owner == NULL)
+	{
+		free(p->name);
+		free(p->owner);
+		free(p);
+		return (NULL);
+	}
 
-    /* Copy the content of name and owner */
-    strcpy(p->name, name);
-    p->age = age;
-    strcpy(p->owner, owner);
+	/* Copy the content of name and owner */
+	strcpy(p->name, name);
+	p->age = age;
+	strcpy(p->owner, owner);
 
-    return (p);
+	return (p);
 }
-
