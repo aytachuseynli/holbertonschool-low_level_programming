ec82538759bad30f3efb70439b08fceb2ba30e82
aytachuseynli <aytac_ha@mail.ru>
2023-12-15 02:20:02 -0800

    first

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -4,6 +4,7 @@
 
 /**
  * read_textfile - prints text from a file
+ *
  * @filename: name of the file
  * @letters: number of characters to read
  *
@@ -11,30 +12,35 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	if (!filename || letters == 0)
+	int file;
+	int length, wrotechars;
+	char *buf;
+
+	if (filename == NULL || letters == 0)
+		return (0);
+	buf = malloc(sizeof(char) * (letters));
+	if (buf == NULL)
 		return (0);
 
-	int file = open(filename, O_RDONLY);
+	file = open(filename, O_RDONLY);
 	if (file == -1)
-		return (0);
-
-	char *buf = malloc(letters);
-	if (!buf)
 	{
-		close(file);
+		free(buf);
 		return (0);
 	}
-
-	ssize_t length = read(file, buf, letters);
-
-	if (length == -1 || write(STDOUT_FILENO, buf, length) != length)
+	length = read(file, buf, letters);
+	if (length == -1)
 	{
 		free(buf);
 		close(file);
 		return (0);
 	}
 
+	wrotechars = write(STDOUT_FILENO, buf, length);
+
 	free(buf);
 	close(file);
+	if (wrotechars != length)
+		return (0);
 	return (length);
 }
